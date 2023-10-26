5733067d386bc767804781f462e59b6eb9b011e1
VictorDino <vblazquez98@gmail.com>
2023-10-27 01:38:43 +0200

    Movimiento, camara, salto y arma
    Scripts movimiento con salto.
    Camara de seguimiento del player.
    Importado modelo de arma (falta disparo, modelo bala y efectos)

---
+++ Assets/Scripts/Camera/CameraLook.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraLook : MonoBehaviour
+{
+    public float mouseSensivity = 80f;
+
+    public Transform playerBody;
+
+    float xRotation = 0;
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+
+        xRotation = Mathf.Clamp(xRotation,-90f,90f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0, 0);
+
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
